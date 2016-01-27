1baab0038464e94576ef741f714945f85112d37e
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-27 13:56:21 +0700

    ENHANCEMENT: Add test for extra attributes

--- tests/GridFieldFormActionTest.php
+++ tests/GridFieldFormActionTest.php
@@ -1,13 +1,51 @@
 <?php
 
 class GridFieldFormActionTest extends SapphireTest {
-	public function testSetExtraAttributes() {
-        $action = new GridFieldFormAction();
-		$this->markTestSkipped('TODO');
-	}
+    /**
+     * @var string
+     */
+    public static $fixture_file = 'blog.yml';
 
-	public function testGetAttributes() {
-		$this->markTestSkipped('TODO');
+    public function setUp() {
+        $this->gridField = new GridField(
+            'BlogPosts',
+            'Blog Posts',
+            BlogPost::get(), // all blog posts, hey it's a test
+            new GridFieldConfig_BlogPost()
+        );
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+	public function testSetExtraAttributes() {
+        $fields = new FieldList(
+            $this->gridField
+        );
+
+        $action = new GridFieldFormAction(
+        $this->gridField,
+            'Name',
+            'Title',
+            'ActionName',
+            array()
+        );
+
+        $actions = new FieldList(
+            $action
+        );
+        $form = new Form(Controller::curr(), 'Form', $fields, $actions);
+        $baseAttributes = $action->getAttributes();
+        $extraAttributes = array(
+            'data-example' => 'an example',
+            'data-another-example' => 'another example'
+        );
+        $action->setExtraAttributes($extraAttributes);
+        $newAttributes = $action->getAttributes();
+        $diff = array_diff($newAttributes, $baseAttributes);
+        $this->assertEquals($extraAttributes, $diff);
 	}
 
 }
