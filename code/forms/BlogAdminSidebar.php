44c045b40acd27541cff8e4d2506b5e9f045f880
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-28 10:45:47 +0700

    WIP: Trying a special case for 0

--- code/forms/BlogAdminSidebar.php
+++ code/forms/BlogAdminSidebar.php
@@ -15,6 +15,10 @@ class BlogAdminSidebar extends FieldGroup
         error_log('T4: $sidebar == null: ' . ($sidebar == null));
         error_log('T5: $sidebar === null: ' . ($sidebar === null));
 
+        // 0 is evaluated as null for PHP < 5.6, so add a special case
+        if ($sidebar === 0) {
+            return false;
+        }
 
         if ($sidebar == 1 || is_null($sidebar)) {
             return true;
