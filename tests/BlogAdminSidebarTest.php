165d25a74c6127bb4ed0ba4af6e7b785757d878a
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-28 17:32:35 +0700

    WIP: Added more trace

--- tests/BlogAdminSidebarTest.php
+++ tests/BlogAdminSidebarTest.php
@@ -11,15 +11,19 @@ class BlogAdminSidebarTest extends SapphireTest {
         // no cookie set
         $this->assertTrue($bar->isOpen());
 
+        error_log('testIsOpen: T1');
         Cookie::set('blog-admin-sidebar',1);
         $this->assertTrue($bar->isOpen());
 
+        error_log('testIsOpen: T2');
         Cookie::set('blog-admin-sidebar',null);
         $this->assertTrue($bar->isOpen());
-        error_log('TESTING 0');
+
+        error_log('testIsOpen: T3');
         Cookie::set('blog-admin-sidebar',0);
         $this->assertFalse($bar->isOpen());
 
+        error_log('testIsOpen: T4');
         Cookie::set('blog-admin-sidebar','fred');
         $this->assertFalse($bar->isOpen());
     }
