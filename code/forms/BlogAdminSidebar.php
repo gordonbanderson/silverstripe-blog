9f440fbbb89ea523501e8b4ef9c1dd41213eed51
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-28 10:19:25 +0700

    WIP: Added value being checked to debug

--- code/forms/BlogAdminSidebar.php
+++ code/forms/BlogAdminSidebar.php
@@ -8,7 +8,7 @@ class BlogAdminSidebar extends FieldGroup
     public function isOpen()
     {
         $sidebar = Cookie::get('blog-admin-sidebar');
-
+        error_log('T0: sidebar is *' . $sidebar .'*');
         error_log('T1: sidebar == 1' . $sidebar == 1);
         error_log('T2: sidebar === 1' . $sidebar === 1);
         error_log('T3: is_null($sidebar)' . is_null($sidebar));
