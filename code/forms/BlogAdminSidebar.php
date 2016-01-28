d801deb2efd1541e769aae4ee5f369b2342ee8d2
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-28 10:08:04 +0700

    WIP: Debugging blog admin side bar checks

--- code/forms/BlogAdminSidebar.php
+++ code/forms/BlogAdminSidebar.php
@@ -9,6 +9,13 @@ class BlogAdminSidebar extends FieldGroup
     {
         $sidebar = Cookie::get('blog-admin-sidebar');
 
+        error_log('T1: sidebar == 1' . $sidebar == 1);
+        error_log('T2: sidebar === 1' . $sidebar === 1);
+        error_log('T3: is_null($sidebar)' . is_null($sidebar));
+        error_log('T4: $sidebar == null' . $sidebar == null);
+        error_log('T5: $sidebar === null' . $sidebar === null);
+
+
         if ($sidebar == 1 || is_null($sidebar)) {
             return true;
         }
