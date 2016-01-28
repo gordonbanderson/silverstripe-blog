d869129db04025f8e3186addadc7fea794fb2796
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-28 10:27:06 +0700

    WIP: Full error trace now working locally

--- code/forms/BlogAdminSidebar.php
+++ code/forms/BlogAdminSidebar.php
@@ -8,12 +8,12 @@ class BlogAdminSidebar extends FieldGroup
     public function isOpen()
     {
         $sidebar = Cookie::get('blog-admin-sidebar');
-        error_log('T0: sidebar is *' . $sidebar .'*');
-        error_log('T1: sidebar == 1' . $sidebar == 1);
-        error_log('T2: sidebar === 1' . $sidebar === 1);
-        error_log('T3: is_null($sidebar)' . is_null($sidebar));
-        error_log('T4: $sidebar == null' . $sidebar == null);
-        error_log('T5: $sidebar === null' . $sidebar === null);
+        error_log('T0: sidebar is *: ' . $sidebar .'*');
+        error_log('T1: sidebar == 1: ' . ($sidebar == 1));
+        error_log('T2: sidebar === 1: ' . ($sidebar === 1));
+        error_log('T3: is_null($sidebar): ' . is_null($sidebar));
+        error_log('T4: $sidebar == null: ' . ($sidebar == null));
+        error_log('T5: $sidebar === null: ' . ($sidebar === null));
 
 
         if ($sidebar == 1 || is_null($sidebar)) {
