859149edefa8b472a0e32794eaedb62f47f12260
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-28 17:32:43 +0700

    WIP: Added more trace

--- code/forms/BlogAdminSidebar.php
+++ code/forms/BlogAdminSidebar.php
@@ -7,13 +7,14 @@ class BlogAdminSidebar extends FieldGroup
      */
     public function isOpen()
     {
+        error_log("=========\nCalling isOpen\n==========");
         $sidebar = Cookie::get('blog-admin-sidebar');
-        error_log('T0: sidebar is *: ' . $sidebar .'*');
-        error_log('T1: sidebar == 1: ' . ($sidebar == 1));
-        error_log('T2: sidebar === 1: ' . ($sidebar === 1));
-        error_log('T3: is_null($sidebar): ' . is_null($sidebar));
-        error_log('T4: $sidebar == null: ' . ($sidebar == null));
-        error_log('T5: $sidebar === null: ' . ($sidebar === null));
+        error_log('T0: sidebar is :*' . $sidebar .'*');
+        error_log('T1: sidebar == 1:' . ($sidebar == 1));
+        error_log('T2: sidebar === 1:' . ($sidebar === 1));
+        error_log('T3: is_null($sidebar):' . is_null($sidebar));
+        error_log('T4: $sidebar == null:' . ($sidebar == null));
+        error_log('T5: $sidebar === null:' . ($sidebar === null));
 
         // 0 is evaluated as null for PHP < 5.6, so add a special case
         if ($sidebar == 0) {
