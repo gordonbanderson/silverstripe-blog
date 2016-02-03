1ff68ed1809e454e4781e1521963f48ce5236d65
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-03 16:38:07 +0700

    WIP: Lots of trace but tests passing locally with Postgres

--- code/widgets/BlogArchiveWidget.php
+++ code/widgets/BlogArchiveWidget.php
@@ -88,37 +88,100 @@ class BlogArchiveWidget extends Widget
      */
     public function getArchive()
     {
+
+        // ---- old code ----
         $query = $this->Blog()->getBlogPosts()->dataQuery();
-        $conn = DB::getConn();
-        if ($this->ArchiveType == 'Yearly') {
-            $yearCond = $conn->formattedDatetimeClause('"BlogPost"."PublishDate"', '%Y');
-            $query->groupBy($yearCond);
-        } else {
-            $yearMonthCond = $conn->formattedDatetimeClause('"BlogPost"."PublishDate"', '%Y-%m');
-            $query->groupBy($yearMonthCond);
-        }
 
         $posts = $this->Blog()->getBlogPosts()->setDataQuery($query);
-
         if ($this->NumberToDisplay > 0) {
             $posts = $posts->limit($this->NumberToDisplay);
         }
 
+        error_log($posts->sql());
+
+        foreach ($posts as $post) {
+            error_log('FROM SQL:' . $post->PublishDate);
+        }
+        //---- old code ----
+
+
+
+
+
+
+
+
+
+
+        $query = $this->Blog()->getBlogPosts()->dataQuery();
+        error_log('CLASS QUERY : ' . get_class($query));
+        $conn = DB::getConn();
+        $timeConstraint = '';
+        if ($this->ArchiveType == 'Yearly') {
+            $timeConstraint = $conn->formattedDatetimeClause('"BlogPost"."PublishDate"', '%Y');
+        //    $query->groupBy($timeConstraint);
+        } else {
+            $timeConstraint = $conn->formattedDatetimeClause('"BlogPost"."PublishDate"', '%Y-%m');
+        //    $query->groupBy($timeConstraint);
+        };
+
+        $query->groupBy($timeConstraint);
+
+
+
+        $posts = $this->Blog()->getBlogPosts()->setDataQuery($query);
+
+
+
+        error_log('SQL: ' . $posts->sql());
+
+        //----------------------------
+        $query = $this->Blog()->getBlogPosts()->dataQuery();
+        $stage = $query->getQueryParam('Versioned.stage');
+        error_log('STAGE T1: ' . $stage);
+
+        if ($stage) {
+            $stage = '_' . $stage;
+        }
+        error_log('STAGE T2: ' . $stage);
+
+        $sql = "SELECT $timeConstraint AS timeconstraint\n";
+        $sql .= "FROM \"SiteTree{$stage}\"\n";
+        $sql .= "LEFT JOIN \"BlogPost{$stage}\" ON \n";
+        $sql .= sprintf('"SiteTree%s"."ID" = "BlogPost%s"."ID"', $stage, $stage);
+        $sql .= "\nWHERE \"SiteTree${stage}\" . \"ParentID\" = " . $this->Blog()->ID;
+        $sql .= "\nGROUP By timeconstraint";
+        $sql .= "\nORDER by timeconstraint DESC\n";
+        if ($this->NumberToDisplay > 0) {
+            $sql .= "\nLIMIT {$this->NumberToDisplay} \n";
+        }
+
+        error_log($sql);
+        $records = DB::query($sql);
+        error_log(print_r($records,1));
+        error_log(sizeof($records));
+        error_log($records->numRecords());
+        foreach ($records as $record) {
+            error_log('RECORD FOUND');
+            error_log($record['timeconstraint']);
+        }
+
+
         $archive = new ArrayList();
 
-        if ($posts->count() > 0) {
-            foreach ($posts as $post) {
-                /**
-                 * @var BlogPost $post
-                 */
-                $date = Date::create();
-                $date->setValue($post->PublishDate);
-
+        if ($records->numRecords() > 0) {
+            foreach ($records as $record) {
                 if ($this->ArchiveType == 'Yearly') {
+                    $date = Date::create();
+                    error_log("**** DATE CLASS ****:" . get_class($date));
+                    $date->setValue($record['timeconstraint'] . '-01-01');
                     $year = $date->FormatI18N("%Y");
                     $month = null;
                     $title = $year;
                 } else {
+                    $time = explode('-', $record['timeconstraint']);
+                    $date = $date = Date::create();
+                    $date->setValue($record['timeconstraint'] . '-01');
                     $year = $date->FormatI18N("%Y");
                     $month = $date->FormatI18N("%m");
                     $title = $date->FormatI18N("%B %Y");
